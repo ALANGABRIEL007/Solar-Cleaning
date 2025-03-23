2cd48f34cd6f7f7472da76148132040d61294c72
ALANGABRIEL007 <107642519+ALANGABRIEL007@users.noreply.github.com>
2025-03-23 20:38:16 +0530

    HUB_WIFI.ino

--- HUB_WIFI.ino
+++ HUB_WIFI.ino
@@ -16,8 +16,8 @@ char password[] = "asdf1234";
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", 19800, 60000); // GMT+5:30 (IST), update every minute
 
-unsigned long lastTriggeredTime = 0;  // To store the last time the broadcast was sent
-unsigned long debounceInterval = 60000; // 1 minute in milliseconds
+unsigned long lastTriggeredTime = 0;  
+unsigned long debounceInterval = 60000; 
 
 int seg_sw = 0;     
 int menu = 0;     
@@ -28,6 +28,14 @@ int threshold = 0;
 unsigned long lastExecutionTime = 0; 
 unsigned long menuInterval = 1000;  
 
+int led = 23  ;
+bool wifiConnected = false;
+unsigned long previousMillis = 0;
+const long blinkInterval = 500; // Blink interval in ms
+bool ledState = false;
+unsigned long wifiConnectedMillis = 0;
+bool ledOnFor3Sec = false;
+
 struct Data{
   int ID;
   int cyc;
@@ -37,26 +45,46 @@ struct Data{
 Data senddata;
 
 void setup() {
-  
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, 16, 17);
+  
+  pinMode(led, OUTPUT);
 
-  // Connect to Wi-Fi
   Serial.println("Connecting to Wi-Fi...");
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nConnected to Wi-Fi!");
-
- 
   timeClient.begin();
-
-  
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, password);
 }
 
+void handleWiFi() {
+  unsigned long currentMillis = millis();
+
+  if (WiFi.status() != WL_CONNECTED) {
+    if (wifiConnected) {
+      wifiConnected = false;
+      Serial.println("\nWi-Fi Connection Lost! Blinking LED...");
+    }
+    
+    if (currentMillis - previousMillis >= blinkInterval) {
+      previousMillis = currentMillis;
+      ledState = !ledState;  
+      digitalWrite(led, ledState);
+    }
+  } else {
+    if (!wifiConnected) {
+      wifiConnected = true;
+      wifiConnectedMillis = millis();
+      ledOnFor3Sec = true;
+      Serial.println("\nConnected to Wi-Fi!");
+      digitalWrite(led, HIGH);
+    }
+
+    if (ledOnFor3Sec && (currentMillis - wifiConnectedMillis >= 3000)) {
+      digitalWrite(led, LOW);
+      ledOnFor3Sec = false;
+    }
+  }
+}
 
 bool isTimeMatch(String targetTime) {
   timeClient.update();
@@ -75,30 +103,25 @@ BLYNK_WRITE(V0) {
   }
 }
 
-
 BLYNK_WRITE(V1) {
   seg_sw = param.asInt();
   Serial.println("seg_sw Updated: " + String(seg_sw));
 }
-
 
 BLYNK_WRITE(V2) {
   menu = param.asInt();
   Serial.println("menu Updated: " + String(menu));
 }
 
-
 BLYNK_WRITE(V3) {
   slot1 = param.asString();
   Serial.println("slot1 Updated: " + slot1);
 }
 
-
 BLYNK_WRITE(V4) {
   slot2 = param.asString();
   Serial.println("slot2 Updated: " + slot2);
 }
-
 
 BLYNK_WRITE(V7) {
   senddata.cyc = param.asInt() + 1;
@@ -108,7 +131,6 @@ BLYNK_WRITE(V7) {
   senddata.ID = 0;
 }
 
-
 BLYNK_WRITE(V8) {
   senddata.thres = param.asInt();
   Serial.print("Threshold Updated: ");Serial.println(senddata.thres);
@@ -116,6 +138,7 @@ BLYNK_WRITE(V8) {
   Serial2.write((uint8_t*)&senddata,sizeof(senddata)); delay(100);
   senddata.ID = 0;
 }
+
 BLYNK_WRITE(V9) {
   senddata.intv = param.asInt();
   Serial.print("Threshold-Interval Updated: ");Serial.println(senddata.intv);
@@ -127,8 +150,8 @@ BLYNK_WRITE(V9) {
 BLYNK_WRITE(V5) {
   Serial.println("Blynk requests temperature");
   senddata.ID = 0;
-  Serial2.write((uint8_t*)&senddata,sizeof(senddata)); delay(100);  // Send temperature request to ESP32 #2
-  
+  Serial2.write((uint8_t*)&senddata,sizeof(senddata)); delay(100);
+
   if (Serial2.available()) {
     String receivedTemperature = Serial2.readStringUntil('\n');
     Serial.println("Temperature received: " + receivedTemperature);
@@ -146,60 +169,40 @@ void sendBroadcast() {
 }
 
 void checkLogic() {
-if (seg_sw == 0) {
-  // Check if 1 minute has passed since the last execution
-  if ((millis() - lastTriggeredTime) >= debounceInterval) {
-    // Check slot1 and slot2
-    if (isTimeMatch(slot1) || isTimeMatch(slot2)) {
-      Serial.println("Specified Time Reached, sending signal");
-      sendBroadcast();
-      
-      // Update the last time the broadcast was triggered
-      lastTriggeredTime = millis();
+  if (seg_sw == 0) {
+    if ((millis() - lastTriggeredTime) >= debounceInterval) {
+      if (isTimeMatch(slot1) || isTimeMatch(slot2)) {
+        Serial.println("Specified Time Reached, sending signal");
+        sendBroadcast();
+        lastTriggeredTime = millis();
+      }
     }
-  }
-} else if (seg_sw == 1) {
-    // Use millis() for non-blocking delay in menu logic
+  } else if (seg_sw == 1) {
     if (millis() - lastExecutionTime >= menuInterval) {
-      lastExecutionTime = millis(); // Update the last execution time
+      lastExecutionTime = millis();
 
       switch (menu) {
-        case 0:
-          menuInterval = 15*60*1000; 
-          Serial.println("15m invterval completed sending signal to bots ");
-          break;
-        case 1:
-          menuInterval = 30*60*1000; 
-          Serial.println("30m invterval completed sending signal to bots ");
-          break;
-        case 2:
-          menuInterval = 1*60*60*1000; 
-          Serial.println("1hr invterval completed sending signal to bots ");
-          break;
-        case 3:
-          menuInterval = 90*60*1000; 
-          Serial.println("1.5hr invterval completed sending signal to bots ");
-          break;
-        case 4:
-          menuInterval = 2*60*60*1000; 
-          Serial.println("2hr invterval completed sending signal to bots ");
-          break;
-        case 5:
-          menuInterval = 3*60*60*1000; 
-          Serial.println("3hr invterval completed sending signal to bots ");
-          break;
-        default:
-          return; 
+        case 0: menuInterval = 15 * 60 * 1000; Serial.println("15m interval completed, sending signal to bots"); break;
+        case 1: menuInterval = 30 * 60 * 1000; Serial.println("30m interval completed, sending signal to bots"); break;
+        case 2: menuInterval = 1 * 60 * 60 * 1000; Serial.println("1hr interval completed, sending signal to bots"); break;
+        case 3: menuInterval = 90 * 60 * 1000; Serial.println("1.5hr interval completed, sending signal to bots"); break;
+        case 4: menuInterval = 2 * 60 * 60 * 1000; Serial.println("2hr interval completed, sending signal to bots"); break;
+        case 5: menuInterval = 3 * 60 * 60 * 1000; Serial.println("3hr interval completed, sending signal to bots"); break;
+        default: return;
       }
-
-      sendBroadcast(); 
+      sendBroadcast();
     }
   }
 }
 
 void loop() {
-  Blynk.run();
-  timeClient.update();
-  checkLogic();
-  delay(1000);
+  handleWiFi();
+
+  if (wifiConnected) {
+    Blynk.run();
+    timeClient.update();
+    checkLogic();
+  }
+
+  delay(10); // Small delay to prevent excessive CPU usage
 }
